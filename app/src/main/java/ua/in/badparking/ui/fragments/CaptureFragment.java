5cd344cf3d9240da586c27e92670851ceaf6fd5b
kovalenych <kovalenych@gmail.com>
2016-07-09 18:08:57 +0300

    camera orientation fixed

--- app/src/main/java/ua/in/badparking/ui/fragments/CaptureFragment.java
+++ app/src/main/java/ua/in/badparking/ui/fragments/CaptureFragment.java
@@ -1,7 +1,5 @@
 package ua.in.badparking.ui.fragments;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -11,18 +9,21 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 import ua.in.badparking.R;
 import ua.in.badparking.ui.MainActivity;
@@ -34,8 +35,11 @@ import ua.in.badparking.ui.utils.CameraPreview;
  */
 public class CaptureFragment extends BaseFragment implements View.OnClickListener {
 
-    private static final int REQUEST_IMAGE_CAPTURE = 356;
-    private static final int PICK_IMAGE = 357;
+//    private static final int REQUEST_IMAGE_CAPTURE = 356;
+//    private static final int PICK_IMAGE = 357;
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
     private static final String TAG = CaptureFragment.class.getName();
 
@@ -51,10 +55,6 @@ public class CaptureFragment extends BaseFragment implements View.OnClickListene
     private File secondImage;
     private View nextButton;
 
-    // Native camera.
-    private Camera mCamera;
-    // View to display the camera output.
-    private CameraPreview mPreview;
 
     public static CaptureFragment newInstance() {
         return new CaptureFragment();
@@ -86,67 +86,149 @@ public class CaptureFragment extends BaseFragment implements View.OnClickListene
         takePhotoButton.setOnClickListener(this);
         nextButton.setOnClickListener(this);
 
-        // Create our Preview view and set it as the content of our activity.
-        boolean opened = safeCameraOpenInView(rootView);
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
 
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(getContext(), mCamera);
+        FrameLayout preview = (FrameLayout)rootView.findViewById(R.id.cameraPreview);
+        preview.addView(mPreview);
         return rootView;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            if (!isFirstHasImage) {
-                if (isSecondHasImage) {
-                    takePhotoButton.setVisibility(View.GONE);
-                }
-                firstHolder.setVisibility(View.VISIBLE);
-                setPic(firstImageView, firstImage.getPath());
-                isFirstHasImage = true;
-                Toast.makeText(getActivity(), R.string.capture_plates, Toast.LENGTH_LONG).show();
-
-            } else {
-                takePhotoButton.setVisibility(View.GONE);
-                secondHolder.setVisibility(View.VISIBLE);
-                setPic(secondImageView, secondImage.getPath());
-                isSecondHasImage = true;
-
-            }
-        } else if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
-            if (getPathFromUri(data.getData()) != null) {
-                File file = new File(getPathFromUri(data.getData()));
-                if (!isFirstHasImage) {
-                    firstHolder.setVisibility(View.VISIBLE);
-                    firstImage = file;
-                    setPic(firstImageView, firstImage.getPath());
-                    isFirstHasImage = true;
-                    Toast.makeText(getActivity(), R.string.capture_plates, Toast.LENGTH_LONG).show();
-                } else {
-                    secondHolder.setVisibility(View.VISIBLE);
-                    secondImage = file;
-                    setPic(secondImageView, secondImage.getPath());
-                    isSecondHasImage = true;
-                }
-            } else
-                Toast.makeText(getActivity(), getString(R.string.error), Toast.LENGTH_LONG).show();
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
         }
+        return c; // returns null if camera is unavailable
     }
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+//            if (!isFirstHasImage) {
+//                if (isSecondHasImage) {
+//                    takePhotoButton.setVisibility(View.GONE);
+//                }
+//                firstHolder.setVisibility(View.VISIBLE);
+//                setPic(firstImageView, firstImage.getPath());
+//                isFirstHasImage = true;
+//                Toast.makeText(getActivity(), R.string.capture_plates, Toast.LENGTH_LONG).show();
+//
+//            } else {
+//                takePhotoButton.setVisibility(View.GONE);
+//                secondHolder.setVisibility(View.VISIBLE);
+//                setPic(secondImageView, secondImage.getPath());
+//                isSecondHasImage = true;
+//
+//            }
+//        } else if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+//            if (getPathFromUri(data.getData()) != null) {
+//                File file = new File(getPathFromUri(data.getData()));
+//                if (!isFirstHasImage) {
+//                    firstHolder.setVisibility(View.VISIBLE);
+//                    firstImage = file;
+//                    setPic(firstImageView, firstImage.getPath());
+//                    isFirstHasImage = true;
+//                    Toast.makeText(getActivity(), R.string.capture_plates, Toast.LENGTH_LONG).show();
+//                } else {
+//                    secondHolder.setVisibility(View.VISIBLE);
+//                    secondImage = file;
+//                    setPic(secondImageView, secondImage.getPath());
+//                    isSecondHasImage = true;
+//                }
+//            } else
+//                Toast.makeText(getActivity(), getString(R.string.error), Toast.LENGTH_LONG).show();
+//        }
+//    }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
+    public void onDestroyView() {
+        super.onDestroyView();
         releaseAll();
-        releaseCameraAndPreview();
+        releaseCamera();              // release the camera immediately on pause event
     }
 
-    public void hideKeyboard(Activity activity) {
-        InputMethodManager inputMethodManager = (InputMethodManager)activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        //Find the currently focused view, so we can grab the correct window token from it.
-        View view = activity.getCurrentFocus();
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = new View(activity);
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
         }
-        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+                Log.d(TAG, "Error creating media file, check storage permissions: ");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    /**
+     * Create a file Uri for saving an image or video
+     */
+    private static Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /**
+     * Create a File for saving an image or video
+     */
+    private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
 
     @Override
@@ -159,8 +241,8 @@ public class CaptureFragment extends BaseFragment implements View.OnClickListene
                 releaseSecondImage();
                 break;
             case R.id.snap:
-//                capture();
-                openGallery();
+                capture();
+//                openGallery();
                 break;
             case R.id.next_button:
                 ((MainActivity)getActivity()).moveToNext();
@@ -172,87 +254,40 @@ public class CaptureFragment extends BaseFragment implements View.OnClickListene
 
     }
 
-    /**
-     * Safe method for getting a camera instance.
-     *
-     * @return
-     */
-    public static Camera getCameraInstance() {
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    /**
-     * Clear any existing preview / camera.
-     */
-    private void releaseCameraAndPreview() {
-
-        if (mCamera != null) {
-            mCamera.stopPreview();
-            mCamera.release();
-            mCamera = null;
-        }
-        if (mPreview != null) {
-            mPreview.destroyDrawingCache();
-            mPreview.mCamera = null;
-        }
-    }
-
-    private boolean safeCameraOpenInView(View view) {
-        boolean qOpened = false;
-        releaseCameraAndPreview();
-        mCamera = getCameraInstance();
-        qOpened = (mCamera != null);
-        mPreview = new CameraPreview(getActivity().getBaseContext(), mCamera);
-        FrameLayout preview = (FrameLayout)view.findViewById(R.id.cameraHolder);
-        preview.addView(mPreview);
-        return qOpened;
-    }
-
     // Photo stuff
 
-    private void openCamera() {
-        if (!isFirstHasImage || !isSecondHasImage) {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-                try {
-                    if (!isFirstHasImage) {
-                        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-                        firstImage = File.createTempFile(String.valueOf(System.currentTimeMillis()), ".jpg", storageDir);
-                    } else {
-                        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-                        secondImage = File.createTempFile(String.valueOf(System.currentTimeMillis()), ".jpg", storageDir);
-                    }
-                } catch (IOException ignored) {
-                }
-                if (firstImage != null || secondImage != null) {
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(!isFirstHasImage ? firstImage : secondImage));
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
-            }
-        } else {
-            Toast.makeText(getActivity(), "You can send only 2 pics", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void openGallery() {
-        if (!isFirstHasImage || !isSecondHasImage) {
-            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(galleryIntent, PICK_IMAGE);
-        } else {
-            Toast.makeText(getActivity(), getString(R.string.only_2_pics), Toast.LENGTH_LONG).show();
-        }
-    }
+//    private void openCamera() {
+//        if (!isFirstHasImage || !isSecondHasImage) {
+//            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+//                try {
+//                    if (!isFirstHasImage) {
+//                        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//                        firstImage = File.createTempFile(String.valueOf(System.currentTimeMillis()), ".jpg", storageDir);
+//                    } else {
+//                        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//                        secondImage = File.createTempFile(String.valueOf(System.currentTimeMillis()), ".jpg", storageDir);
+//                    }
+//                } catch (IOException ignored) {
+//                }
+//                if (firstImage != null || secondImage != null) {
+//                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(!isFirstHasImage ? firstImage : secondImage));
+//                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//                }
+//            }
+//        } else {
+//            Toast.makeText(getActivity(), "You can send only 2 pics", Toast.LENGTH_LONG).show();
+//        }
+//    }
+//
+//    private void openGallery() {
+//        if (!isFirstHasImage || !isSecondHasImage) {
+//            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//            startActivityForResult(galleryIntent, PICK_IMAGE);
+//        } else {
+//            Toast.makeText(getActivity(), getString(R.string.only_2_pics), Toast.LENGTH_LONG).show();
+//        }
+//    }
 
     private void releaseFirstImage() {
         firstImageView.setImageBitmap(null);
